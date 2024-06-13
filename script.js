61f945eab65ba7dde8fa4d1fce12c2084a799c54
Mithun Kumar H S <115037566+Mithunkumarhs@users.noreply.github.com>
2024-06-13 15:00:58 +0530

    Added

---
+++ script.js
@@ -0,0 +1,44 @@
+//toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+//scroll section
+let sections = document.querySelectorAll('section');
+let navlink = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navlink.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+            //active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        //if wnat to use animation on scroll
+        sec.classList.remove('show-animate')
+    });
+  
+    // Sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar on click
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
